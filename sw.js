26ecda1fa6fe14267ff00e917815e8f985d3d88a
tolgayilmaz1 <77330117+tolgayilmaz1@users.noreply.github.com>
2026-05-17 11:08:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'witherfight-v1';
+const FILES = [
+  '/witherfight/',
+  '/witherfight/index.html',
+  '/witherfight/manifest.json',
+  '/witherfight/icon-192.png',
+  '/witherfight/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
